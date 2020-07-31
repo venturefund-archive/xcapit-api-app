4b33a5f45300fbb71c62f0e0bce61517278671d7
Martin Brunori <martinbrunori@MacBook-Pro.local>
2020-07-31 15:59:44 -0300

    :sparkles: feat (admin): create django admin

--- terms_and_conditions/admin.py
+++ terms_and_conditions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from terms_and_conditions.models import TermsAndConditions
 
 # Register your models here.
+class TermsAndConditionsAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'accepted_at')
+
+admin.site.register(TermsAndConditions, TermsAndConditionsAdmin)
