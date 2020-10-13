644dd2c9fa21ef944842fbe20fce29ae3caf00ba
Franco Schillage <franco@xcapit.com>
2020-10-13 12:55:54 +0000

    :sparkles: feat: SX-435 - Redirigir en base a si tienen referidos

--- referrals/urls.py
+++ referrals/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ReferralsViewSet
+from .views import ReferralsViewSet, ReferralsCountView
 
 app_name = 'referrals'
 
 urlpatterns = [
     path('user/<user_id>', ReferralsViewSet.as_view({'post': 'create', 'get': 'retrieve'}), name="referrals"),
+    path('count/user/<user_id>', ReferralsCountView.as_view(), name="count-referrals")
 ]
