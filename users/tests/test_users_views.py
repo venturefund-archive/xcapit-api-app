f5c3016202e7fe179d89e61c1af855290d5fbd32
Franco Schillage <franco@xcapit.com>
2021-04-26 20:20:22 +0000

    :bug: fix: Rollback on refactor, and creation of new endpoint

--- users/tests/test_users_views.py
+++ users/tests/test_users_views.py
@@ -4,21 +4,30 @@ from unittest.mock import patch
 from rest_framework import status
 
 
-@pytest.mark.wip
 @pytest.mark.django_db
-@pytest.mark.parametrize('email, user_is_active, expected_status, expected_response', [
-    ['test@test.com', False, status.HTTP_200_OK, {}],
-    ['test@test.com', True, status.HTTP_400_BAD_REQUEST,
+@pytest.mark.parametrize('user_is_active, data, expected_status, expected_response', [
+    [False, {'uidb64': 'MQ'}, status.HTTP_200_OK, {}],
+    [True, {'uidb64': 'MQ'}, status.HTTP_400_BAD_REQUEST,
      {'error_code': 'users.sendEmailValidationToken.userAlreadyActive'}],
-    ['testqwd@test.com', False, status.HTTP_400_BAD_REQUEST, {'error_code': 'users.sendEmailValidationToken.user'}]])
+    [False, {'uidb64': 'XXX'}, status.HTTP_400_BAD_REQUEST,
+     {'error_code': 'users.sendEmailValidationToken.user'}],
+    [False, {'email': 'test@test.com'}, status.HTTP_200_OK, {}],
+    [True, {'email': 'test@test.com'}, status.HTTP_400_BAD_REQUEST,
+     {'error_code': 'users.sendEmailValidationToken.userAlreadyActive'}],
+    [False, {'email': 'testqwd@test.com'}, status.HTTP_400_BAD_REQUEST,
+     {'error_code': 'users.sendEmailValidationToken.user'}],
+    [False, {}, status.HTTP_400_BAD_REQUEST,
+     {'non_field_errors': ['Debe existir al menos uno de los campos requeridos: email, uidb64']}],
+    [False, {'email': 'test@test.com', 'uidb64': 'MQ'}, status.HTTP_400_BAD_REQUEST,
+     {'non_field_errors': ['Los campos son mutuamente exclusivos']}]])
 @patch('users.emails.EmailValidation.notifications_client.send_email_validation')
-def test_send_email_validation_token(mock_send_email, client, create_user, email, user_is_active, expected_status,
+def test_send_email_validation_token(mock_send_email, client, create_user, user_is_active, data, expected_status,
                                      expected_response):
     mock_send_email.status_code = 200
     create_user(email='test@test.com', password="test", is_active=user_is_active)
     response = client.post(
         reverse('users:send-email-validation'),
-        data={"email": email},
+        data=data,
         content_type='application/json'
     )
     assert response.status_code == expected_status
