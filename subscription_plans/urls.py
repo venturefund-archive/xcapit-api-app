1f4dfdab0f257c891b98d53a6c487735ccc27bd7
Maximiliano Cesar Nivoli <gringonivoli@gmail.com>
2021-07-12 15:44:40 -0300

    :bug: fix: Add / to post endpoint

--- subscription_plans/urls.py
+++ subscription_plans/urls.py
@@ -5,5 +5,5 @@ app_name = 'subscription_plans'
 
 urlpatterns = [
     path('plans/<plan_id>/payment_methods', PaymentMethodsByPlanAPIView.as_view(), name='payment_methods_by_plan'),
-    path('free_subscription', FreePlanSubscriptionAPIView.as_view(), name='create_free_subscription'),
+    path('free_subscription/', FreePlanSubscriptionAPIView.as_view(), name='create_free_subscription'),
 ]
