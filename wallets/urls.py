4003bd33817df76318974fdc6cdbebf72b5ac0be
Franco Schillage <fschillage@gmail.com>
2021-11-02 11:43:57 -0300

    fix: Add missing slash in post url

--- wallets/urls.py
+++ wallets/urls.py
@@ -5,6 +5,6 @@ app_name = 'wallets'
 
 urlpatterns = [
     path('user/<user_id>/', WalletsView.as_view(), name='wallets'),
-    path('request_nft/<user_id>', CreateNFTRequestView.as_view(), name='create-nft-request'),
+    path('request_nft/<user_id>/', CreateNFTRequestView.as_view(), name='create-nft-request'),
     path('get_nft_status/<user_id>', NFTStatusView.as_view(), name='nft-status')
 ]
