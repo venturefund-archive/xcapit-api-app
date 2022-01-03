678535bd4cc9133c989321114ae2a8bfa9c5e154
Franco Schillage <fschillage@gmail.com>
2022-01-03 11:12:29 -0300

    feat: Add investor_category and investor_score to serializer

---
+++ profiles/tests/test_get_profile.py
@@ -0,0 +1,11 @@
+import pytest
+from users.models import User
+from django.urls import reverse
+
+
+@pytest.mark.django_db
+def test_get_investor_category_from_profile(client, test_user):
+    url = reverse('profiles:retrieve-update-user-profile', kwargs={'user_id': test_user.id})
+    response = client.get(url)
+    assert response.status_code == 200
+    assert response.json()['investor_category'] == 'wealth_managements.profiles.no_category'
--- profiles/tests/test_update_profile.py
+++ profiles/tests/test_update_profile.py
@@ -5,24 +5,43 @@ from users.models import User
 
 
 @pytest.mark.django_db
-def test_update_notifications_enabled_patch(client):
-    user = User.objects.create_user('test', 'test')
-    assert user.profile.notifications_enabled
+def test_update_notifications_enabled_patch(client, test_user):
+    assert test_user.profile.notifications_enabled
     response = client.patch(
         reverse('profiles:retrieve-update-user-profile', kwargs={'user_id': 1}),
         data={'notifications_enabled': False},
         content_type='application/json'
     )
     assert response.status_code == 200
-    assert not Profile.objects.get(user_id=user.id).notifications_enabled
+    assert not Profile.objects.get(user_id=test_user.id).notifications_enabled
 
 
 @pytest.mark.django_db
-def test_update_notifications_enabled_patch_invalid(client):
-    User.objects.create_user('test', 'test')
+def test_update_notifications_enabled_patch_invalid(client, test_user):
     response = client.patch(
         reverse('profiles:retrieve-update-user-profile', kwargs={'user_id': 1}),
         data={'notifications_enabled': 'yes, sure'},
+        content_type='application/json'
+    )
+    assert response.status_code == 400
+
+
+@pytest.mark.django_db
+def test_update_investor_score_patch(client, test_user):
+    response = client.patch(
+        reverse('profiles:retrieve-update-user-profile', kwargs={'user_id': 1}),
+        data={'investor_score': 6},
+        content_type='application/json'
+    )
+    assert response.status_code == 200
+    assert Profile.objects.get(user_id=test_user.id).investor_score == 6
+
+
+@pytest.mark.django_db
+def test_update_investor_score_patch_invalid(client, test_user):
+    response = client.patch(
+        reverse('profiles:retrieve-update-user-profile', kwargs={'user_id': 1}),
+        data={'investor_score': 19},
         content_type='application/json'
     )
     assert response.status_code == 400
