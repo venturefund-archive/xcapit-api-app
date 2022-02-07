847f1fed89b67905d41d2bc0407783e6ad64b418
Maximiliano Cesar Nivoli <gringonivoli@gmail.com>
2022-02-07 19:01:20 -0300

    feat: WIP filter referrals

--- referrals/referral_count_of.py
+++ referrals/referral_count_of.py
@@ -1,10 +1,18 @@
+from core.datetime.datetime_range import DatetimeRange
 from referrals.next_level_referrals import NextLevelReferrals
 
 
-class ReferralCountOf():
-    def __init__(self, referrals: NextLevelReferrals, wallet: bool):
+# TODO: may be a FilteredReferrals object will has the filtering logic 🤔
+# FilterdedReferrals(ReferralCountOf, DatetimeRange).value() 🤔
+class ReferralCountOf:
+    def __init__(self, referrals: NextLevelReferrals, wallet: bool, a_datetime_range: DatetimeRange = DatetimeRange()):
+        self._a_datetime_range = a_datetime_range
         self._referrals = referrals
         self._wallet = wallet
 
     def value(self):
-        return len(self._referrals.all()[self._referrals.all()['wallet_created'] == self._wallet])
+        all_ = self._referrals.all()[self._referrals.all()['wallet_created'] == self._wallet]
+        # TODO: should I create a StringOf with since().value()/to().value()
+        all_ = all_[all_['created_at'] >= self._a_datetime_range.since().value()] if self._a_datetime_range.since() else all_
+        all_ = all_[all_['created_at'] <= self._a_datetime_range.to().value()] if self._a_datetime_range.to() else all_
+        return len(all_)
