4cec4e16e024ba3f51c2125f313fc12bc09ca884
Franco Schillage <franco@xcapit.com>
2022-05-26 12:00:54 -0300

    feat: Update load investor test survey command

--- surveys/tests/test_models.py
+++ surveys/tests/test_models.py
@@ -22,10 +22,10 @@ def test_create_survey_with_questions_and_answers(create_survey):
 @pytest.mark.django_db
 def test_create_question_with_same_order_in_same_survey_raises_exception(create_survey):
     with pytest.raises(IntegrityError):
-        Question.objects.create(survey=Survey.objects.get(name='investor_test'), text='test', order=0)
+        Question.objects.create(survey=Survey.objects.get(name='investor_test'), order=0)
 
 
 @pytest.mark.django_db
 def test_create_question_with_same_order_in_new_survey_ok(create_survey):
     different_survey = Survey.objects.create(name='SurveyTwo')
-    assert Question.objects.create(survey=different_survey, text='test', order=0)
+    assert Question.objects.create(survey=different_survey, order=0)
